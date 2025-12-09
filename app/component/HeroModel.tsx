5e82a7d4f56bcd3542e18e6523f0a9289cf2cf7e
panggilsajarey <150323912+raynard05@users.noreply.github.com>
2025-12-09 08:12:46 +0700

    add loader

--- app/component/HeroModel.tsx
+++ app/component/HeroModel.tsx
@@ -1,25 +1,36 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { OrbitControls, useGLTF, Html } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useRef, Suspense } from "react";
 import * as THREE from "three";
 import "@/app/component/Hero/Hero.css";
+
 function Model() {
-  const gltf = useGLTF("/astronout4.glb"); // ganti sesuai model kamu
+  const gltf = useGLTF("/astronout4.glb");
   const ref = useRef<THREE.Group>(null);
 
-  // Animasi bounce lembut, pelan, loop
   useFrame((state) => {
     const t = state.clock.getElapsedTime();
     if (ref.current) {
-      ref.current.position.y = Math.sin(t * 1) * 0.1; // bounce pelan
-      ref.current.rotation.y = t * 0.01; // rotasi halus
+      ref.current.position.y = Math.sin(t * 1) * 0.1;
+      ref.current.rotation.y = t * 0.4; 
     }
   });
 
   return <primitive ref={ref} object={gltf.scene} scale={0.7} />;
+}
+
+function YellowBlackLoader() {
+  return (
+    <Html center>
+      <div className="loader-wrapper">
+        <div className="loader-spinner"></div>
+        <p className="loader-text">Loading 3D Asset...</p>
+      </div>
+    </Html>
+  );
 }
 
 export default function HeroModel() {
@@ -28,9 +39,11 @@ export default function HeroModel() {
       <Canvas camera={{ position: [0, 0, 2], fov: 45 }}>
         <ambientLight intensity={1} />
         <directionalLight position={[3, 3, 3]} />
-        <Model />
 
-        {/* Optional: kontrol mouse */}
+        <Suspense fallback={<YellowBlackLoader />}>
+          <Model />
+        </Suspense>
+
         <OrbitControls enableZoom={false} />
       </Canvas>
     </div>
