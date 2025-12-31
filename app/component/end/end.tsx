2110391eb28bbb935ce8c578309a688e52b140be
raynard05 <ahmadraynardaurelio@gmail.com>
2025-12-31 11:16:38 +0700

    add optimize

--- app/component/end/end.tsx
+++ app/component/end/end.tsx
@@ -15,16 +15,15 @@ export default function EndPage() {
       <MaskContainer
         revealSize={maskSize}
         revealText={
-          <p className="mx-auto max-w-4xl text-center text-4xl font-bold text-slate-800 dark:text-yellow-400 px-4">
+          <p className="mx-auto max-w-4xl text-center text-4xl font-bold text-[#FFD000] px-4">
             Let's Build Something Amazing Together
           </p>
         }
         className="w-full h-full"
       >
-        <span className="text-5xl text-black">
-          Ready to turn ideas into reality? Whether it's a <span className="text-white"> web app, mobile solution,
-            or IoT innovation, </span> I'm here to bring your <span className="text-white">vision to life. </span>
-
+        <span className="text-5xl text-black font-bold">
+          Ready to turn ideas into reality? Whether it's a web app, mobile solution,
+          or IoT innovation, I'm here to bring your vision to life.
         </span>
       </MaskContainer>
     </section>
